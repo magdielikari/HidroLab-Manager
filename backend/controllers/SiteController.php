85c6fa28e5e554e0a7a959893c51b8e17c0d6c3a
Nosthertus <darkfreak17@hotmail.com>
2015-10-27 18:58:06 -0430

    Listo para master

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -26,7 +26,7 @@ class SiteController extends Controller
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index', 'test'],
+                        'actions' => ['logout', 'index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -79,10 +79,5 @@ class SiteController extends Controller
         Yii::$app->user->logout();
 
         return $this->goHome();
-    }
-
-    public function actionTest()
-    {
-        return $this->render('test');
     }
 }
